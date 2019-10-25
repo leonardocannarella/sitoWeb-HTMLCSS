90136fd2aba8d032d42cb8dc83b8838f19c28ebc
Leonardo Cannarella <leonardocannarella11@gmail.com>
2019-10-25 10:21:03 +0200

    Add files via upload
    - Aggiunta Dark Mode
    - Aggiunti interruttori per cambiare modalità

---
+++ script.js
@@ -0,0 +1,31 @@
+function day(){
+	body.className='day';
+    header.className='day';
+    content.className='day';
+    titolo.className='day';
+    descrizione.className='day';
+    citazione.className='day';
+    elenco.className='day';
+    interruttore.className='day';
+    citazioneImportante.className='day';
+    citazioneImportante1.className='day';
+    citazioneImportante2.className='day';
+    galleriafoto.className='day';
+    footer.className='day';
+}
+
+function night(){
+    body.className='night';
+    header.className='night';
+    content.className='night';
+    titolo.className='night';
+    descrizione.className='night';
+    citazione.className='night';
+    elenco.className='night';
+    interruttore.className='night';
+    citazioneImportante.className='night';
+    citazioneImportante1.className='night';
+    citazioneImportante2.className='night';
+    galleriafoto.className='night';
+    footer.className='night';
+}
